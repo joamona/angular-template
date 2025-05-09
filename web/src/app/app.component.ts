9b2ff1a437ba4f89acb481fcc89d454357baaa36
joamona <joamona@cgf.upv.es>
2025-05-09 13:09:29 +0200

    m

--- web/src/app/app.component.ts
+++ web/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {MatButtonToggleModule} from '@angular/material/button-toggle';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatButtonToggleModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
